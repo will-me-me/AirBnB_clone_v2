ee9561cf217d3abd7160a4d2a18dfeafe9073257
The-alpha-male <ronnymunene@outlook.com>
2024-01-17 05:38:31 -0800

    changed storage engine to SqlAlchemy

--- models/city.py
+++ models/city.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """ City Module instances cities with ids """
-from sqlalchemy import Column, Integer, String, ForeignKey
-from models.base_model import BaseModel, Base
-from os import getenv
-from sqlalchemy.orm import relationship
 
-STORAGE = getenv("HBNB_TYPE_STORAGE")
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class City(BaseModel, Base):
-    """The city class, contains state ID and name"""
-
+    """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    if STORAGE == "db":
-        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities", cascade="all, delete-orphan")
-    else:
-        name = ""
-        state_id = ""
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', cascade='all, delete, delete-orphan',
+                          backref='cities')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
