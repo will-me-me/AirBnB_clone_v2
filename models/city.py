c0b1a0676094387d06e841384f90267ab90a2379
will-me-me <willykaria777@gmail.com>
2024-01-17 01:48:28 -0800

    updated the city model

--- models/city.py
+++ models/city.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models import storage_type
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from os import getenv
-from models import storage_type
 
 
-
-class City(BaseModel):
-    """The city class, contains state ID and name"""
-    __tablename__ = "cities"
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship("Place", backref="cities", cascade="all, delete")
+        places = relationship('Place', backref='cities',
+                              cascade='all, delete, delete-orphan')
     else:
         state_id = ""
         name = ""
-
